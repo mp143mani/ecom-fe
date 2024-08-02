2319f5d872bf314a80c0cd77d2f6398ce94194b6
mp143mani <mani143tech@gmail.com>
2024-08-02 13:08:56 +0530

    s

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    backendUrl: process.env.REACT_APP_API || 'https://eccomerce-s49e.onrender.com/'
+    backendUrl: 'https://eccomerce-s49e.onrender.com'
   };
   
   export default config;
